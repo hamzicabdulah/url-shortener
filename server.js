d62ad3e8a13401377a2050d2a588e55dffa06fe4
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-02-25 20:49:31 +0000

    Added copy-pasted temporary index file

--- server.js
+++ server.js
@@ -48,6 +48,8 @@ MongoClient.connect(URL, function(err, db) {
     });
   });
   
+  app.use(express.static(__dirname + '/public'));
+  
   var port = process.env.PORT || 8080;
   app.listen(port, function() {
     console.log('Listening on port ' + port);
