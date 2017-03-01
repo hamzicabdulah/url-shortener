0d29d9ce8122726ab9a48524dd372aaa65e4596d
Abdulah Hamzic <abdulahhamzic@gmail.com>
2017-03-01 19:54:07 +0000

    Styled index file

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ MongoClient.connect(URL, function(err, db) {
   if (err) return console.log(err);
   var collection = db.collection('urls');
   
-  app.get('/https?://*.*', function(req, res) {
+  app.get('/new/https?://*.*', function(req, res) {
     //If parameter is a legit url, first check to see whether it already exists in the database collection
-    collection.find({original_url: req.originalUrl.slice(1)}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, docs) {
+    collection.find({original_url: req.originalUrl.slice(5)}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, docs) {
       if (err) throw err;
       if (docs.length < 1) {
         //If url doesn't exist in the collection, generate a random short url and add it to the collection, then display it
         var randNum = parseInt(Math.random() * 9999);
-        collection.insert({original_url: req.originalUrl.slice(1), short_url: "https://" + req.headers.host + "/" + randNum}, function(err) {
+        collection.insert({original_url: req.originalUrl.slice(5), short_url: "https://" + req.headers.host + "/" + randNum}, function(err) {
           if (err) return console.log(err);
-          collection.find({original_url: req.originalUrl.slice(1)}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, docs) {
+          collection.find({original_url: req.originalUrl.slice(5)}, {_id: 0, original_url: 1, short_url: 1}).toArray(function(err, docs) {
             if (err) throw err;
             console.log("Added " + docs[0].original_url + " to database");
             res.send(docs[0]);
@@ -48,7 +48,13 @@ MongoClient.connect(URL, function(err, db) {
     });
   });
   
-  app.use(express.static(__dirname + '/public'));
+  app.get('/new/:something', function(req, res) {
+    //If the url format is wrong, display a json error
+    console.log("Invalid url inserted");
+    res.send({error: "Wrong url format, make sure you have a valid protocol and real site."});
+  });
+  
+  app.use(express.static(__dirname + '/client'));
   
   var port = process.env.PORT || 8080;
   app.listen(port, function() {
